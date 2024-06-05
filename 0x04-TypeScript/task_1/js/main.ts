75f819929ea80ec323428e6315ecaeeb29c6eb43
Youssef <youssefkaramedu@gmail.com>
2024-06-05 20:10:42 +0000

    Add task 2

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly fullTimeEmployee: boolean;
+  readonly yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; 
+}
