b9ae85318f30c02a4a8c19f73d5f652540ebb5fd
Youssef <youssefkaramedu@gmail.com>
2024-07-22 17:58:06 +0000

    Add file 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.exit();
+	if (name)
+	{
+  	  console.log(`Your name is: ${name}`);
+	}
 });
 
 process.stdin.on('end', () => {
