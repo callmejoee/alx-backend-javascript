9b063c3323c19fbea2ffecf356d090f8e22e190b
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-07-22 21:25:01 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  if (name) {
-    console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
