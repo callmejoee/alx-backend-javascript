a479f9b13f2ff2821f88c2e415c911896c1e7d79
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-07-26 05:13:18 +0300

    Create 4-payment.test.js

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with SUM and log the correct message', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    stub.restore();
+    consoleSpy.restore();
+  });
+});
