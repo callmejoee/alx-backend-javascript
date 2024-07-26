0f7e65be17c4488feb2178f3ee2fce5bd091ec58
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-07-26 14:23:58 +0300

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,27 +1,5 @@
 const request = require('request');
 const { expect } = require('chai');
-<<<<<<< HEAD
-const server = require('./api');
-
-describe('Index page', function() {
-  before(function(done) {
-    // Ensure server is running
-    done();
-  });
-
-  after(function(done) {
-    // Close server after tests
-    server.close(done);
-  });
-
-  it('should return the correct message', function(done) {
-    request('http://localhost:7865', (error, response, body) => {
-      if (error) {
-        return done(error);
-      }
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-=======
 
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
@@ -30,7 +8,6 @@ describe('API integration test', () => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
->>>>>>> b2b61481f12afb7993775c6df571c3757b285f24
       done();
     });
   });
