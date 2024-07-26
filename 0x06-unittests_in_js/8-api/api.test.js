b2b61481f12afb7993775c6df571c3757b285f24
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-07-26 05:37:47 +0300

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,36 +1,14 @@
 const request = require('request');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
 
-const BASE_URL = 'http://localhost:7865';
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index page', () => {
-  let server;
-
-  before((done) => {
-    server = require('./api'); // Import the server
-    server.listen(7865, done); // Start the server
-  });
-
-  after((done) => {
-    server.close(done); // Close the server after tests
-  });
-
-  it('should return status code 200', (done) => {
-    request.get(`${BASE_URL}/`, (err, res) => {
-      if (err) return done(err);
-      expect(res.statusCode).to.equal(200);
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
-
-  it('should return the correct message', (done) => {
-    request.get(`${BASE_URL}/`, (err, res, body) => {
-      if (err) return done(err);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  // Additional tests can be added here
 });
