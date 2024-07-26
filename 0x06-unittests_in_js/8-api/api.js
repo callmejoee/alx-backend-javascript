4ead266ea7aadacc8cae4b657cfe400dd59fae4d
Youssef <youssefkaramedu@gmail.com>
2024-07-26 02:43:19 +0000

    Add file 9

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 7865;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+const server = app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
 });
 
-module.exports = app;
+module.exports = server;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,36 +1,26 @@
 const request = require('request');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
+const server = require('./api');
 
-const BASE_URL = 'http://localhost:7865';
-
-describe('Index page', () => {
-  let server;
-
-  before((done) => {
-    server = require('./api'); // Import the server
-    server.listen(7865, done); // Start the server
+describe('Index page', function() {
+  before(function(done) {
+    // Ensure server is running
+    done();
   });
 
-  after((done) => {
-    server.close(done); // Close the server after tests
+  after(function(done) {
+    // Close server after tests
+    server.close(done);
   });
 
-  it('should return status code 200', (done) => {
-    request.get(`${BASE_URL}/`, (err, res) => {
-      if (err) return done(err);
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct message', (done) => {
-    request.get(`${BASE_URL}/`, (err, res, body) => {
-      if (err) return done(err);
+  it('should return the correct message', function(done) {
+    request('http://localhost:7865', (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
-
-  // Additional tests can be added here
 });
