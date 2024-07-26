03cf1e1014fa9165a34b101b826658778f54b939
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-07-26 14:23:29 +0300

    Update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
+const app = express();
+const PORT = 7865;
+
+app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
-const server = app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
-module.exports = server;
+module.exports = app;
